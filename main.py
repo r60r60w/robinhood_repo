f26a566bf0b0a17c3bbc661b9e99587d608fd76b
r60r60w <jhuang1869@gmail.com>
2024-11-24 22:18:39 -0800

    Minor commit

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ logger = get_logger(__name__)
 def main():
     login(days=1)
     mode = 'normal'
-    symbol_list=['GOOGL','TSM','INTC','AMD','NVDA'
+    symbol_list=['GOOGL','TSM','INTC','AMD','NVDA']
     trader = OptionTrader(symbol_list=[], mode=mode)
     trader.run_cc(risk_level='medium', delta=0.25, MAX_ATTEMPT=3)
 
