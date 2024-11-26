e55b0bc9d15d4fb5991d2ea20b3422b6744bfca6
r60r60w <jhuang1869@gmail.com>
2024-11-26 10:30:19 -0800

    Refactored main function to improve readability and updated login days to 3

--- main.py
+++ main.py
@@ -2,11 +2,9 @@
 from jh_options import *
 from jh_utilities import *
 from jh_optionTrader import *
-import json
+
 logger = get_logger(__name__)
 
-with open('settings.json', 'r') as f:
-    settings = json.load(f)
 
 #%%
 '''TODO:
@@ -14,14 +12,13 @@ with open('settings.json', 'r') as f:
  2. Reduce price carefully instead of hard code minus 1 cent
 '''
 def main():
-    login(days=1)
-    mode = settings['mode']
-    symbol_list=settings['symbol_list']
-    risk_level=settings['risk_level']
-    delta = settings['delta']
-    only_manage_existing = settings['only_manage_existing']
-    trader = OptionTrader(symbol_list=symbol_list, mode=mode)
-    trader.run_cc(risk_level=risk_level, delta=delta, MAX_ATTEMPT=3, only_manage_existing=only_manage_existing)
+    login(days=3)
+    trader = OptionTrader(symbol_list=settings['symbol_list'], mode=settings['mode'])
+    trader.run_cc(risk_level=settings['risk_level'],
+                  delta=settings['delta'], 
+                  MAX_ATTEMPT=3, 
+                  only_manage_existing=settings['only_manage_existing']
+                  )
 
 
 if __name__ == "__main__":
